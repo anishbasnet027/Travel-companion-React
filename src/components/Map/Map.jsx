bc2f1a5d311178f70ba66db7dc865186cdced03c
Anish Basnet <anishbasnet027@gmail.com>
2022-12-29 16:49:13 +0545

    first commit

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
+
+import mapStyles from "../../mapStyles";
+import useStyles from "./styles.js";
+
+const Map = () => {
+  return <div>Map</div>;
+};
+
+export default Map;
